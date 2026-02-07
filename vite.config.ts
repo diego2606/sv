f7f120a982c233bf8db44e376b6376523a0e75a8
diego2606 <diego.velasquez.ore@gmail.com>
2026-02-07 02:48:15 -0500

    Fix asset paths for GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react"
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "")
   return {
-    base: "/sv/", // Cambia 'valentine' por el nombre de tu repositorio
+    base: "/sv", // Cambia 'valentine' por el nombre de tu repositorio
     server: {
       port: 3000,
       host: "0.0.0.0",
